55d2d30d18dfde0231503b2c015ac7ce895f214c
oneplateosc <165935110+oneplateosc@users.noreply.github.com>
2024-06-27 14:18:43 +0900

    ユーザー管理機能2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@
 FactoryBot.define do
   factory :user do
     nickname { Faker::Name.unique.first_name }
-    last_name { "やマ田" }
-    first_name { "太ろウ" }
-    last_name_kana { "ヤマダ" }
-    first_name_kana { "タロウ" }
+    last_name { 'やマ田' }
+    first_name { '太ろウ' }
+    last_name_kana { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
     birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
     email { Faker::Internet.unique.email }
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
   end
 end
