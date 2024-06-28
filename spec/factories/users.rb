849485180ab7c1597f014471f7b6cba392d7d173
oneplateosc <165935110+oneplateosc@users.noreply.github.com>
2024-06-28 18:45:02 +0900

    ユーザー管理機能3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     first_name_kana { 'タロウ' }
     birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
     email { Faker::Internet.unique.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -100,6 +100,27 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
+      it "パスワードが半角英字のみでは登録できない" do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      
+      it "パスワードが半角数字のみでは登録できない" do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      
+      it "パスワードが全角文字を含む場合は登録できない" do
+        @user.password = 'ｐａｓｓｗｏｒｄ１'
+        @user.password_confirmation = 'ｐａｓｓｗｏｒｄ１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
       it '重複したメールアドレスが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
